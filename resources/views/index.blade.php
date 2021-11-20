dcd2d3a27706cbbd41133f16b9acfefa48e6aaf1
yusaazmi <yusaazmi24@gmail.com>
2021-11-20 19:05:30 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+</head>
+<body style="background-color: #2b2d2e">
+    <div class="container-xxl mt-2 mx-2" style="background-color: rgb(46, 46, 59)">
+        <div class="row">
+            <div class="col-lg-3 pt-4">
+                <div class="card position-fixed" style="width:300px;background-color: rgb(62, 62, 71)">
+                    <div class="d-flex justify-content-center">
+                        <div class="p-2">
+                            <img src="{{asset('assets/azmi.jpg')}}" class="card-img-top rounded-circle" style="width:120px;height:120px;">
+                        </div>
+                    </div>
+                    <div class="card-body">
+                      <p class="card-text text-center" style="font-weight: 600;font-size:20px;color:white">Yusa Azmi</p>
+                      <p class="card-text text-center" style="font-weight: 400;font-size:14px;color:#8C8C8E">Web Developer</p>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-lg-8 pl-0">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="container mt-5" style="background-color:rgb(92, 92, 99)">
+                            <div class="row px-5 py-5">
+                                <div class="col-lg-12">
+                                    <h3 style="color:white;font-weight:600">I am Yusa Azmi</h3>
+                                </div>
+                                <div class="col-lg-12 mt-2">
+                                    <h4 style="color:white;font-weight:600">Web Developer</h4>
+                                </div>
+                                <div class="col-lg-12 mt-2">
+                                    <p style="color:white">With strong professional background in software development, web development, blockchain technical consulting and development, decentralized application development, ICO advisor. Have been a long experienced veteran in blockchain field since its initial development. Have educated skills and expertise in software engineering, computer science, fundamental principles of cryptography and blockchain technologies in full-lifecycle work across multiple technologies. Love and enthusiastic for Ehereum development, expect its thrilling future. Very flexible, fast learner, proven ability to adapt to new technologies.</p>
+                                </div>
+                                <div class="col-lg-12 mt-2">
+                                    <button class="btn btn-warning text-black rounded-0 btn-lg" style="font-weight:500;font-size:16px;">EXPLORE NOW</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 mt-4">
+                        <div class="container">
+                            <div class="d-flex justify-content-between">
+                                <div class="p-2">
+                                    <p style="font-weight: 600;color:yellow;font-size:20px;"><strong>100</strong> Customer</p>
+                                </div>
+                                <div class="p-2">
+                                    <p style="font-weight: 600;color:yellow;font-size:20px;"><strong>100</strong> Customer</p>
+                                </div>
+                                <div class="p-2">
+                                    <p style="font-weight: 600;color:yellow;font-size:20px;"><strong>100</strong> Customer</p>
+                                </div>
+                                <div class="p-2">
+                                    <p style="font-weight: 600;color:yellow;font-size:20px;"><strong>100</strong> Customer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-lg-12">
+                        <h4 style="color:white">My Services</h4>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="card shadow-lg" style="background-color:rgba(58, 58, 75, 0.877)">
+                                    <div class="card-body">
+                                        <p style="color:white;font-weight:400">Product Dev. / Mangement</p>
+                                        <p style="color: rgb(121, 121, 121)">I transform an idea into a beautiful and highly functional product to meet business and user needs.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="card shadow-lg" style="background-color:rgba(58, 58, 75, 0.877)">
+                                    <div class="card-body">
+                                        <p style="color:white;font-weight:400">Product Dev. / Mangement</p>
+                                        <p style="color: rgb(121, 121, 121)">I transform an idea into a beautiful and highly functional product to meet business and user needs.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-lg-12">
+                        <h4 style="color:white">Recomendations</h4>
+                        <div class="row mt-5">
+                            <div class="col-lg-6">
+                                <div class="card shadow-lg" style="background-color:rgba(58, 58, 75, 0.877)">
+                                    <div class="card-body">
+                                        <p style="color:white;font-weight:400">Gorga Simanullang</p>
+                                        <i style="color: rgb(121, 121, 121)">CEO of Pro Worker</i>
+                                        <p style="color: rgb(121, 121, 121)">I transform an idea into a beautiful and highly functional product to meet business and user needs.</p>
+                                    </div>
+                                </div>
+                                <img src="{{asset('assets/azmi.jpg')}}" class="rounded-circle" style="position: absolute;width:75px;height:75px;right:30px;top:-20px">
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="card shadow-lg" style="background-color:rgba(58, 58, 75, 0.877)">
+                                    <img src="{{asset('assets/azmi.jpg')}}" class="rounded-circle position-absolute" style="width:75px;height:75px;right:10px;bottom:90px;top:-10">
+                                    <div class="card-body">
+                                        <p style="color:white;font-weight:400">Product Dev. / Mangement</p>
+                                        <p style="color: rgb(121, 121, 121)">I transform an idea into a beautiful and highly functional product to meet business and user needs.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1 pt-4 pr-3">
+                <div class="d-flex justify-content-center">
+                    <div class="p-2">
+                        <button style="background-color: rgba(46, 46, 59, 0.877);border:none;"><i class="fas fa-grip-lines fa-lg"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script>
+    function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementByClass("value");
+animateValue(obj, 100, 0, 5000);
+</script>
+</body>
+</html>
